26585b7b3da80b2fb323d2f875f93d781ffaa49a
unknown <reza.wikrama3@gmail.com>
2015-07-28 19:07:14 +0700

    add  oblagio config

---
+++ app/OblagioConfig/config.php
@@ -0,0 +1,14 @@
+<?php namespace oblagio\OblagioConfig;
+
+    class config
+    {
+            public static function main()
+            {
+                    return [
+                            'applicationName' => 'Oblagio Task',
+                            'routeBackend' => 'admin-cp',
+                            'routeGenerator' => 'obgl',
+                    ];
+            }
+       
+    }
