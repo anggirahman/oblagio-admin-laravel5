23df02dea13f154356a8c4e696232ef4caca0e8e
unknown <reza.wikrama3@gmail.com>
2015-07-28 19:30:58 +0700

    change unique validation on update action method

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -13,13 +13,13 @@ class Role extends Model
     public function rules()
     {
         $uri = Request::segment(3);
-        
         if($uri == 'create')
         {
             return ['title' => 'required|alpha|unique:roles'];
         
         }elseif($uri == 'update'){
-            return ['title' => 'required|alpha|unique:roles,id,'.Request::segment(4)];
+            $model = $this->find(Request::segment(4));
+            return ['title' => 'required|alpha|unique:roles,title,'.$model->id];
         }
     }
 }
