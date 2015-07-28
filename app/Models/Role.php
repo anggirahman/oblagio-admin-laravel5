1efadce94285690dc7e208ff8f395d2c5ade002f
unknown <reza.wikrama3@gmail.com>
2015-07-28 18:14:43 +0700

    change validation rules in Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,13 +3,23 @@
 namespace oblagio\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Request;
 
 class Role extends Model
 {
     public $guarded = [];
     
-    public $rules = [
-        'title' => 'required|alpha|unique:roles',
-    ];
+    
+    public function rules()
+    {
+        $uri = Request::segment(3);
+        
+        if($uri == 'create')
+        {
+            return ['title' => 'required|alpha|unique:roles'];
+        
+        }elseif($uri == 'update'){
+            return ['title' => 'required|alpha|unique:roles,id,'.Request::segment(4)];
+        }
+    }
 }
